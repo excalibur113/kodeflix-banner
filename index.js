e71dc330c1a708ce62fb30f2ce21aff51250fc91
excalibur113 <tabucanonej@gmail.com>
2023-01-24 12:50:56 +0800

    version2.0

--- index.js
+++ index.js
@@ -1,30 +1,48 @@
-function validation () {
-  var form = document.getElementById("form")
-  var email = document.getElementById("email").value;
-  var text = document.getElementById("text");
-  var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
-if (email.match(pattern)){
-  form.classList.add("valid");
-  form.classList.remove("invalid");
-  text.innerHTML = "";
-  text.style.color = "#ffa00a";
-}
-else{
-  form.classList.remove("valid");
-  form.classList.add("invalid");
-  text.innerHTML = "Please enter a valid email address";
-  text.style.color = "#ffa00a";
-}
-if (email == ""){
-  form.classList.remove("valid");
-  form.classList.remove("invalid");
-  text.innerHTML = "Email is required";
-  text.style.color = "#ffa00a";
+// function validation () {
+//   var form = document.getElementById("form")
+//   var email = document.getElementById("email").value;
+//   var text = document.getElementById("text");
+//   var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+// if (email.match(pattern)){
+//   form.classList.add("valid");
+//   form.classList.remove("invalid");
+//   text.innerHTML = "";
+//   text.style.color = "#ffa00a";
+// }
+// else{
+//   form.classList.remove("valid");
+//   form.classList.add("invalid");
+//   text.innerHTML = "Please enter a valid email address";
+//   text.style.color = "#ffa00a";
+// }
+// if (email == ""){
+//   form.classList.remove("valid");
+//   form.classList.remove("invalid");
+//   text.innerHTML = "Email is required";
+//   text.style.color = "#ffa00a";
 
-}
-}
+// }
+// }
 
+const register = document.querySelector('#register');
+const email = document.querySelector('#email');
 
+const emailError = document.querySelector('#emailError');
+
+const validateEmail = () => {
+  const emailValue = email.ariaValueMax.trim();
+  const emailValidator = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+  emailError.innerHTML = "";
+
+  if( !emailValue){
+    emailError.innerText = 'Email is required!';
+  
+  
+  }
+  else if ( !emailValidator.test(emailValue)){
+    emailError.innerText = 'Please enter a valid email';
+  }
+}
 
 function toggle1(){
   var x = document.getElementById('ans1');
